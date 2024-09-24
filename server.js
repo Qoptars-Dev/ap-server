334c00c2231a444f740628f45444918994b16ace
Qoptars-Dev <qoptarsdevteam@gmail.com>
2024-09-24 17:02:24 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import ImageObject from '../server/models/ImageObject.js'; // Import the model for 'imageObject'
+import ImageObject from './models/ImageObject.js'; // Import the model for 'imageObject'
 import dotenv from 'dotenv';
 
 dotenv.config(); // Initialize dotenv
